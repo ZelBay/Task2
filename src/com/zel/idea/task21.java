445352f8f209581728ad3c50c9fc4b7532f704d2
ZelBay <zelbay@mail.ru>
2021-05-10 12:37:31 +0300

    task2

---
+++ src/com/zel/idea/task21.java
@@ -0,0 +1,9 @@
+package com.zel.idea;
+
+public class task21 {
+    public static void main (String[] args) {
+        double num = 234.84512369784563d;
+
+        System.out.print(num);
+    }
+}
---
+++ src/com/zel/idea/task22.java
@@ -0,0 +1,10 @@
+package com.zel.idea;
+
+public class task22 {
+    public static void main(String[] args) {
+        byte num = 127;
+
+        System.out.print(num);
+    }
+
+}
