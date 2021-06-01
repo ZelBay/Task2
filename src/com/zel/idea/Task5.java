c894ae21abaa59486802395d1081940f8b656ed8
ZelBay <zelbay@mail.ru>
2021-06-01 20:25:06 +0300

    Task5

--- src/com/zel/idea/Task5.java
+++ src/com/zel/idea/Task5.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Task5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
         String ch = scanner.next();
         switch (ch) {
             case "+": System.out.println(a + b);
@@ -15,7 +15,13 @@ public class Task5 {
                 break;
             case "*": System.out.println(a * b);
                 break;
-            case "/": System.out.println(a / b);
+            case "/":
+                if (b == 0) {
+                    System.out.println("Низя на 0 делить, ты чё");
+                }
+                else {
+                    System.out.println(a / b);
+                }
                 break;
             default:  System.out.println("DEFAULT");
 
