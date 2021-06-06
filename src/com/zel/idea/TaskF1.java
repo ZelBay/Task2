9795657343546d6a46eefdc521d22fa39f73569e
ZelBay <zelbay@mail.ru>
2021-06-06 22:38:03 +0300

    TaskF1

--- src/com/zel/idea/TaskF1.java
+++ src/com/zel/idea/TaskF1.java
@@ -1,5 +1,16 @@
 package com.zel.idea;
 
+import java.util.Scanner;
+
 public class TaskF1 {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите курс доллара: ");
+        double dollarRate = scanner.nextDouble();
+        System.out.println("Введите количество рублей: ");
+        int rubleCount = scanner.nextInt();
+        double result = rubleCount / dollarRate;
+        System.out.printf("Итого: %.2f долларов", result);
+    }
 
 }
